40036048ba787d0264e84506dea62f4fdfcee034
Paulalemany <palemany@ucm.es>
2024-11-21 19:04:43 +0100

    TERMINADA ESCENA DEL VIENTO

--- skeleton/VientoScene.cpp
+++ skeleton/VientoScene.cpp
@@ -3,8 +3,8 @@
 VientoScene::VientoScene()
 {
 	//Añadimos las fuerzas
-	fuerzas.push_back(new VientoGenerator(Vector3{-50,0,-20}, Vector3(50, 0, 0)));
-	fuerzas[0]->setRadio(100);
+	fuerzas.push_back(new VientoGenerator(Vector3{-50,0,-20}, Vector3(20, 0, -20)));
+	fuerzas[0]->setRadio(100);	//Hacemos que la zona sea grande
 }
 
 VientoScene::~VientoScene()
@@ -13,4 +13,13 @@ VientoScene::~VientoScene()
 
 void VientoScene::update(double t)
 {
+	//Vamos generando particulas para que se lleve el viento
+	if (particulas.size() < 5) {
+		Proyectil* p = new Proyectil({ -50, 0, -20 }, { 0,0,0 }, { 0,0,0 }, 0.5);
+		p->setLiveTime(500);
+		p->setMasa(0.1);
+		particulas.push_back(p);
+	}
+
+	Scene::update(t);
 }

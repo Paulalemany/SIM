25d21ab1f23a902300259c96ea4f3eba9f4e9dcb
Paulalemany <palemany@ucm.es>
2024-11-24 13:21:38 +0100

    Si pulsas la v hay viento que mueve la particula

--- skeleton/AnclaScene.cpp
+++ skeleton/AnclaScene.cpp
@@ -1,8 +1,10 @@
 #include "AnclaScene.h"
 
 AnclaScene::AnclaScene()
-{
+{	
 	//Creamos dos particulas, el ancla y la que se mueve
+	fuerzas.push_back(new VientoGenerator({ 0,0,0 }, { 10, 10, 0 }));
+
 	Proyectil* p = new Proyectil({ -10,20,0 }, { 0,0,0 }, { 0,0,0 }, 0.85);
 	AnclaFG* ancla = new AnclaFG({ 10, 10, 0 }, 5, 10, { 10.0, 20, 0 });
 	ancla->setRadio(100);
@@ -22,4 +24,40 @@ AnclaScene::~AnclaScene()
 void AnclaScene::keyPressed(unsigned char key, const physx::PxTransform& camera)
 {
 	//Queremos que se ejecute una fuerza
+	//Podemos poner un viento que se ejecute tocando la v
+
+	switch (key)
+	{
+	case 'v':
+		//No se si esto funciona en mi cabeza esto lo invierte
+		viento = !viento;
+		break;
+	default:
+		break;
+	}
+}
+
+void AnclaScene::update(double t)
+{
+	///Actualizamos los sistemas que haya
+	if (active) {
+		for (auto sys : sistemas) sys->update(t);
+
+		for (auto p : particulas) {
+
+			//La primera de fuerzas solo lo hacemos si esta activo
+			if (viento) {
+				fuerzas[0]->update(t);
+				if (fuerzas[0]->onZone(p->getPosition())) p->addForce(fuerzas[0]->generateForce(*p));
+			}
+
+			//Hacemos la fuerza del muelle
+			fuerzas[1]->update(t);
+			if (fuerzas[1]->onZone(p->getPosition())) p->addForce(fuerzas[1]->generateForce(*p));
+			
+			p->update(t);
+		}
+
+		eliminaPart();
+	}
 }
--- skeleton/AnclaScene.h
+++ skeleton/AnclaScene.h
@@ -2,6 +2,7 @@
 #include "Scene.h"
 #include "MuelleForeGenerator.h"
 #include "MuelleParticleGenerator.h"
+#include "VientoGenerator.h"
 
 class AnclaScene : public Scene
 {
@@ -11,9 +12,14 @@ public:
 
 	void keyPressed(unsigned char key, const physx::PxTransform& camera) override;
 
+	//Tenemos que hacer nuestro propio update para la fuerza del viento
+	void update(double t) override;
+
 protected:
-	ParticleGenerator* generator = nullptr;
+	ForceGenerator* generator = nullptr;
 	AnclaFG* ancla;
+
+	bool viento = false;
 
 };
 

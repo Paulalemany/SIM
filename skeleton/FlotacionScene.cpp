b0b1569ae20cc56528d798766a2f64db70d073cd
Paulalemany <palemany@ucm.es>
2024-11-29 10:05:45 +0100

    Creo que ya esta bien

--- skeleton/FlotacionScene.cpp
+++ skeleton/FlotacionScene.cpp
@@ -3,7 +3,7 @@
 FlotacionScene::FlotacionScene()
 {
 	//Ponemos la fuerza
-	fuerzas.push_back(new FlotacionForce({ 0,30,0 }, 5, 1, 1000));
+	fuerzas.push_back(new FlotacionForce({ 0,30,0 }, 10, 1, 1000));
 
 	//creamos la particula que flota
 	Proyectil* p = new Proyectil({ 0,40,0 }, { 0,0,0 }, { 0,0,0 }, 0.85);

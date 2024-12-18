8ea2221826b0e42437a0625a80fe07804e7e919b
Paulalemany <palemany@ucm.es>
2024-12-18 12:49:13 +0100

    Tenemos bala
    Ahora toca diseñar el nivel a ver que se me ocurre. Lo de empujarlas va a ser más complicado creo yo asi que distintos tipos de rebote podrian estar bien

--- skeleton/NivelSolidos.cpp
+++ skeleton/NivelSolidos.cpp
@@ -19,7 +19,7 @@ void NivelSolidos::shoot(Vector3 pos)
 	if (solidBullet != nullptr) solidBullet->~SolidoRigido();	//Solo queremos una bala en escena
 
 	//La direccion es el vector que hay entre el punto pos y el origen
-	Vector3 ori = { -20, 0, 0 };
+	Vector3 ori = { -30, 0, 0 };
 	Vector3 dir = (pos - ori).getNormalized();
 
 	//Esta vez la bala es un solido dinamico
